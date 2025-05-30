7aede462a20f6012f9d950b4fdfc24cf308150c8
matvey <immoraljudgment@gmail.com>
2025-05-30 18:16:05 +0300

    изменен app

--- app/app.py
+++ app/app.py
@@ -171,8 +171,8 @@ def format_phone(number):
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
 
 #@app.route('/search')
 #def search():
